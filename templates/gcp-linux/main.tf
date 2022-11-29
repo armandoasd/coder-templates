bada775c80fcdaf968158ece2f2db5ad42c8c265
Armando Lesaj <arlesaj@deloittece.com>
2022-11-29 16:10:42 +0100

    Update main.tf

--- templates/gcp-linux/main.tf
+++ templates/gcp-linux/main.tf
@@ -85,7 +85,7 @@ resource "coder_app" "code-server" {
   slug         = "code-server"
   display_name = "code-server"
   icon         = "/icon/code.svg"
-  url          = "http://localhost:13337?folder=/home/coder"
+  url          = "http://localhost:13337?folder=/home/${data.coder_workspace.me.owner}/workspace"
   subdomain    = false
   share        = "owner"
 
